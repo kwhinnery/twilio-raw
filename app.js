e05e8ca0e2ccc92cfe886abc7a9542aa943afccd
Kevin Whinnery <kevin.whinnery@gmail.com>
2013-01-02 17:23:07 -0600

    need else block

--- app.js
+++ app.js
@@ -56,10 +56,11 @@ http.createServer(function (req, res) {
             }
         });
     }
-
-    res.writeHead(200, {
-        'Content-Type':'text/plain'
-    });
-    res.end('send a POST.');
+    else {
+        res.writeHead(200, {
+            'Content-Type':'text/plain'
+        });
+        res.end('send a POST.');
+    }
 
 }).listen(port);
