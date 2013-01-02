7c16cbfb98b704009666558891e3615ecedb2ed3
Kevin Whinnery <kevin.whinnery@gmail.com>
2013-01-02 16:34:17 -0600

    take two for heroku

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var twilio = require('twilio'),
-http = require('http');
+    http = require('http');
+
+var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	res.end('Hello World\n');
-	}).listen(process.env.port || 8080);
+    res.writeHead(200, {
+        'Content-Type':'text/plain'
+    });
+    res.end('Hello World\n');
+}).listen(port);
