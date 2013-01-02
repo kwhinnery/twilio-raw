3f3c1881b506bf7026a130bc50ff227e0886b90c
Kevin Whinnery <kevin.whinnery@gmail.com>
2013-01-02 16:47:21 -0600

    use twilio from github

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var twilio = require('twilio'),
+var config = require('config'),
+    twilio = require('twilio'),
     http = require('http');
 
 var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
+    //hard code this for heroku deployment
+    twilio.validateRequest(config.authToken, res.he)
+
+    var resp = new twilio.TwimlResponse();
+    resp.say('hello world!');
+
     res.writeHead(200, {
-        'Content-Type':'text/plain'
+        'Content-Type':'text/xml'
     });
     res.end('Hello World\n');
 }).listen(port);
